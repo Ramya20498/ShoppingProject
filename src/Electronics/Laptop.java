daac13cd17aee20a83e24b7ad0466d9d99c9a9fa
Ramya <Ramyasandra20@gmail.com>
2022-06-07 23:51:41 +0530

    this is my second commit

--- src/Electronics/Laptop.java
+++ src/Electronics/Laptop.java
@@ -5,7 +5,7 @@ public class Laptop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("buying the new laptop");
-
+        System.out.println("updating latest version");
 	}
 
 }
