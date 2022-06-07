479a797172d64681824d16ff7a39c9923b0b6568
Ramya <Ramyasandra20@gmail.com>
2022-06-07 23:48:00 +0530

    This is My first commit

---
+++ src/Electronics/Laptop.java
@@ -0,0 +1,11 @@
+package Electronics;
+
+public class Laptop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("buying the new laptop");
+
+	}
+
+}
