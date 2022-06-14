4cbcdd433c0061f6920e39d98ed36fa6c8414d2a
Ramya <Ramyasandra20@gmail.com>
2022-06-14 22:16:35 +0530

    updating the version 2.3 commit

--- src/Electronics/Laptop.java
+++ src/Electronics/Laptop.java
@@ -6,6 +6,8 @@ public class Laptop {
 		// TODO Auto-generated method stub
 		System.out.println("buying the new laptop");
         System.out.println("updating latest version");
+        System.out.println("updating version 1.2");
+        System.out.println("updating the version 2.3 ");
 	}
 
 }
